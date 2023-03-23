2333888c92fac55eed7270db1219928e06ba887a
Max Glaser <hello@mpfglaser.nl>
2023-03-23 11:17:14 +0000

    add example launch file

--- pi/robot_workspace/src/noonoo/setup.py
+++ pi/robot_workspace/src/noonoo/setup.py
@@ -1,3 +1,5 @@
+from glob import glob
+import os
 from setuptools import setup
 
 package_name = 'noonoo'
@@ -8,8 +10,10 @@ setup(
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+         ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share',
+                      package_name), glob('launch/*launch.[pxy][yma]*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -19,7 +23,6 @@ setup(
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
-        'console_scripts': [
-        ],
+        'console_scripts': [],
     },
 )
