fe3f7280e700becf0eb923bb8db877c500825458
Max Glaser <hello@mpfglaser.nl>
2023-03-29 10:34:41 +0000

    update launch file

--- pi/robot_workspace/launch/noonoo_launch.py
+++ pi/robot_workspace/launch/noonoo_launch.py
@@ -4,10 +4,14 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
-        Node(package='cv2serial',
-             executable='node',
+        # Node(package='cv2serial',
+        #      executable='node',
+        #      output='screen',
+        #      name='node'),
+        Node(package='serial_bridge',
+             executable='serial_bridge',
              output='screen',
-             name='node'),
+             name='serial_bridge'),
         Node(
             name='rplidar_composition',
             package='rplidar_ros',
