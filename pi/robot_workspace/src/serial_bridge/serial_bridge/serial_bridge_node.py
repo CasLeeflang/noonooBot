a73a9219292f68ba3d79309115bc4f42d6dd9a8c
CasLeeflang <466933@student.fontys.nl>
2023-03-30 10:59:49 +0200

    TD cleanup

--- pi/robot_workspace/src/serial_bridge/serial_bridge/serial_bridge_node.py
+++ pi/robot_workspace/src/serial_bridge/serial_bridge/serial_bridge_node.py
@@ -5,7 +5,6 @@ from nav_msgs.msg import Odometry
 import serial
 import struct
 
-
 class serialBridgeNode(Node):
 
     logSerialWrite = False
@@ -14,27 +13,41 @@ class serialBridgeNode(Node):
     def __init__(self):
         super().__init__('serial_bridge_node')
 
+        self.serial_port = '/dev/ttyACM0'
+        self.serial_msg_length = 14
+        self.serial_com_time = 0.05
+
         # ROS topic --[data]--> Serial device
         self.subscription = self.create_subscription(Twist, 'cmd_vel',
-                                                     self.write_serial, 10)
+                                                     self.write_serial, 
+                                                     10)
 
         # Serial device --[data]--> ROS topic
-        self.publisher = self.create_publisher(Odometry, 'odom', 10)
+        self.publisher = self.create_publisher(Odometry, 
+                                               'odom', 
+                                               10)
 
-        timer_period = 0.05  # seconds
-        self.timer = self.create_timer(timer_period, self.read_serial)
+        self.timer = self.create_timer(self.serial_com_time, self.read_serial)
 
-        self.get_logger().info('Starting serial bridge node')
-        self.subscription
-        self.ser = serial.Serial('/dev/ttyACM0')
+        self.get_logger().info('Initializing serial bridge node')
+        self.ser = serial.Serial(self.serial_port)
         self.ser.baudrate = 115200
-
         self.ser.reset_input_buffer()
+
+    def unpack_serial_msg(msg):
+        odometry_msg = Odometry()
+
+        odometry_msg.pose.pose.position.x = struct.unpack(
+            '<f', msg[0:4])[0]
+        odometry_msg.pose.pose.position.y = struct.unpack(
+            '<f', msg[4:8])[0]
+        odometry_msg.pose.pose.orientation.z = struct.unpack(
+            '<f', msg[8:12])[0]
+        return odometry_msg
 
     def write_serial(self, msg):
 
         sending = bytearray()
-
         sending += struct.pack('<f', msg.linear.x)
         sending += struct.pack('<f', msg.angular.z)
         sending.append(0x0d)
@@ -47,17 +60,10 @@ class serialBridgeNode(Node):
             self.get_logger().info('Angular: "%s"' % msg.angular.z)
 
     def read_serial(self):
-        while (self.ser.in_waiting >= 14):
+        while (self.ser.in_waiting >= self.serial_msg_length):
             received_message = self.ser.read_until(b'\r\n')
 
-            odometry_msg = Odometry()
-
-            odometry_msg.pose.pose.position.x = struct.unpack(
-                '<f', received_message[0:4])[0]
-            odometry_msg.pose.pose.position.y = struct.unpack(
-                '<f', received_message[4:8])[0]
-            odometry_msg.pose.pose.orientation.z = struct.unpack(
-                '<f', received_message[8:12])[0]
+            odometry_msg = self.unpack_serial_msg(received_message)
 
             self.publisher.publish(odometry_msg)
 
@@ -65,7 +71,6 @@ class serialBridgeNode(Node):
                 self.get_logger().info(
                     f'Current position: \n X: {str(odometry_msg.pose.pose.position.x)[:3]} Y: {str(odometry_msg.pose.pose.position.y)[:3]} Z-orientation: {str(odometry_msg.pose.pose.orientation.z)[:3]}'
                 )
-
             return
 
 
