ccfc8455b5bc1d6aa734f1b2fa9cb8b9c4f2c6e5
CasLeeflang <71006841+CasLeeflang@users.noreply.github.com>
2023-03-31 10:35:02 +0200

    Techdeb 0330 (#8)
    * TD cleanup
    
    * Parameters and functions

--- pi/robot_workspace/src/serial_bridge/serial_bridge/serial_bridge_node.py
+++ pi/robot_workspace/src/serial_bridge/serial_bridge/serial_bridge_node.py
@@ -5,59 +5,71 @@ from nav_msgs.msg import Odometry
 import serial
 import struct
 
-
 class serialBridgeNode(Node):
 
-    logSerialWrite = False
-    logSerialRead = True
+  
 
     def __init__(self):
         super().__init__('serial_bridge_node')
 
-        # ROS topic --[data]--> Serial device
+        self.logSerialWrite = False
+        self.logSerialRead = True
+        self.serial_port = '/dev/ttyACM0'
+        self.serial_msg_length = 14
+        self.serial_com_time = 0.05
+        self.baudrate = 115200
+
         self.subscription = self.create_subscription(Twist, 'cmd_vel',
-                                                     self.write_serial, 10)
+                                                     self.write_serial, 
+                                                     10)
 
-        # Serial device --[data]--> ROS topic
-        self.publisher = self.create_publisher(Odometry, 'odom', 10)
+        self.publisher = self.create_publisher(Odometry, 
+                                               'odom', 
+                                               10)
 
-        timer_period = 0.05  # seconds
-        self.timer = self.create_timer(timer_period, self.read_serial)
-
-        self.get_logger().info('Starting serial bridge node')
+        self.timer = self.create_timer(self.serial_com_time, 
+                                       self.read_serial)
+        
+        self.publisher
         self.subscription
-        self.ser = serial.Serial('/dev/ttyACM0')
-        self.ser.baudrate = 115200
 
+        self.get_logger().info('Initializing serial bridge node')
+        self.ser = serial.Serial(self.serial_port)
+        self.ser.baudrate = self.baudrate
         self.ser.reset_input_buffer()
 
-    def write_serial(self, msg):
+    def unpack_serial_msg(msg):
+        odometry_msg = Odometry()
 
+        odometry_msg.pose.pose.position.x = struct.unpack(
+            '<f', msg[0:4])[0]
+        odometry_msg.pose.pose.position.y = struct.unpack(
+            '<f', msg[4:8])[0]
+        odometry_msg.pose.pose.orientation.z = struct.unpack(
+            '<f', msg[8:12])[0]
+        return odometry_msg
+    
+    def pack_serial_msg(msg):
         sending = bytearray()
-
         sending += struct.pack('<f', msg.linear.x)
         sending += struct.pack('<f', msg.angular.z)
         sending.append(0x0d)
         sending.append(0x0a)
+        return sending
 
-        self.ser.write(sending)
+
+    def write_serial(self, msg):
+        self.ser.write(self.pack_serial_msg(msg))
 
         if self.logSerialWrite:
             self.get_logger().info('Linear: "%s"' % msg.linear.x)
             self.get_logger().info('Angular: "%s"' % msg.angular.z)
 
     def read_serial(self):
-        while (self.ser.in_waiting >= 14):
+        while (self.ser.in_waiting >= self.serial_msg_length):
             received_message = self.ser.read_until(b'\r\n')
 
-            odometry_msg = Odometry()
-
-            odometry_msg.pose.pose.position.x = struct.unpack(
-                '<f', received_message[0:4])[0]
-            odometry_msg.pose.pose.position.y = struct.unpack(
-                '<f', received_message[4:8])[0]
-            odometry_msg.pose.pose.orientation.z = struct.unpack(
-                '<f', received_message[8:12])[0]
+            odometry_msg = self.unpack_serial_msg(received_message)
 
             self.publisher.publish(odometry_msg)
 
@@ -65,7 +77,6 @@ class serialBridgeNode(Node):
                 self.get_logger().info(
                     f'Current position: \n X: {str(odometry_msg.pose.pose.position.x)[:3]} Y: {str(odometry_msg.pose.pose.position.y)[:3]} Z-orientation: {str(odometry_msg.pose.pose.orientation.z)[:3]}'
                 )
-
             return
 
 
