87a5ed18eec2aa4df0c45611c348f1444d86f324
CasLeeflang <466933@student.fontys.nl>
2023-04-03 17:20:40 +0200

    init

---
+++ robot_workspace/launch/noonoo_launch.py
@@ -0,0 +1,51 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+
+    urdf_file_name = 'robot.urdf'
+    urdf = os.path.join(
+        get_package_share_directory('robot_description'),
+        urdf_file_name)
+    with open(urdf, 'r') as infp:
+        robot_desc = infp.read()
+        
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use simulation (Gazebo) clock if true'),
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'use_sim_time': use_sim_time, 'robot_description': robot_desc}],
+            arguments=[urdf]),
+   
+        Node(package='serial_bridge',
+             executable='serial_bridge',
+             output='screen',
+             name='serial_bridge'),
+        Node(
+            name='rplidar_composition',
+            package='rplidar_ros',
+            executable='rplidar_composition',
+            output='screen',
+            parameters=[{
+                'serial_port': '/dev/ttyUSB0',
+                'serial_baudrate': 115200,  # A1 / A2
+                'frame_id': 'laser',
+                'inverted': False,
+                'angle_compensate': True,
+            }],
+        ),
+        
+    ])
---
+++ robot_workspace/src/serial_bridge/serial_bridge/serial_bridge_node.py
@@ -0,0 +1,86 @@
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
+import serial
+import struct
+
+class serialBridgeNode(Node):
+
+  
+
+    def __init__(self):
+        super().__init__('serial_bridge_node')
+
+        self.logSerialWrite = False
+        self.logSerialRead = True
+        self.serial_port = '/dev/ttyACM0'
+        self.serial_msg_length = 14
+        self.serial_com_time = 0.05
+        self.baudrate = 115200
+
+        self.subscription = self.create_subscription(Twist, 'cmd_vel',
+                                                     self.write_serial, 
+                                                     10)
+
+        self.publisher = self.create_publisher(Odometry, 
+                                               'odom', 
+                                               10)
+
+        self.timer = self.create_timer(self.serial_com_time, 
+                                       self.read_serial)
+        
+        self.publisher
+        self.subscription
+
+        self.get_logger().info('Initializing serial bridge node')
+        self.ser = serial.Serial(self.serial_port)
+        self.ser.baudrate = self.baudrate
+        self.ser.reset_input_buffer()
+
+
+    def write_serial(self, msg):
+
+        sending = bytearray()
+        sending += struct.pack('<f', msg.linear.x)
+        sending += struct.pack('<f', msg.angular.z)
+        sending.append(0x0d)
+        sending.append(0x0a)
+        self.ser.write(sending)
+
+        if self.logSerialWrite:
+            self.get_logger().info('Linear: "%s"' % msg.linear.x)
+            self.get_logger().info('Angular: "%s"' % msg.angular.z)
+
+    def read_serial(self):
+        while (self.ser.in_waiting >= self.serial_msg_length):
+            received_message = self.ser.read_until(b'\r\n')
+
+            odometry_msg = Odometry()
+
+            odometry_msg.pose.pose.position.x = struct.unpack(
+                '<f', received_message[0:4])[0]
+            odometry_msg.pose.pose.position.y = struct.unpack(
+                '<f', received_message[4:8])[0]
+            odometry_msg.pose.pose.orientation.z = struct.unpack(
+                '<f', received_message[8:12])[0]
+
+            self.publisher.publish(odometry_msg)
+
+            if self.logSerialRead:
+                self.get_logger().info(
+                    f'Current position: \n X: {str(odometry_msg.pose.pose.position.x)[:3]} Y: {str(odometry_msg.pose.pose.position.y)[:3]} Z-orientation: {str(odometry_msg.pose.pose.orientation.z)[:3]}'
+                )
+            return
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    serial_bridge_node = serialBridgeNode()
+    rclpy.spin(serial_bridge_node)
+    serialBridgeNode.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
