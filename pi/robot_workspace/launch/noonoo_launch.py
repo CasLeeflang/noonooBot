8e19c1077af1eaa112a154568048562c76a90750
Max Glaser <hello@mpfglaser.nl>
2023-04-04 08:14:46 +0000

    remove redundant cv2serial node

--- pi/robot_workspace/launch/noonoo_launch.py
+++ pi/robot_workspace/launch/noonoo_launch.py
@@ -4,10 +4,6 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
-        # Node(package='cv2serial',
-        #      executable='node',
-        #      output='screen',
-        #      name='node'),
         Node(package='serial_bridge',
              executable='serial_bridge',
              output='screen',
@@ -15,8 +11,7 @@ def generate_launch_description():
         Node(package='tfbroadcaster',
              executable='tfbroadcaster',
              output='screen',
-             name='tfbroadcaster'
-             ),
+             name='tfbroadcaster'),
         Node(
             name='rplidar_composition',
             package='rplidar_ros',
